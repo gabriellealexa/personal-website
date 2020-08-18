6d939ba318f1b5dbc8556c7cdbbb6a07894d3107
github username <github email address>
2020-08-18 18:24:35 -0400

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  render(){
+    return (
+<div className="header">
+    <div className="typing-header">
+      Hi, I'm Gabrielle.
+    </div>
+</div>
+    )};
+  }
+  export default Header;
